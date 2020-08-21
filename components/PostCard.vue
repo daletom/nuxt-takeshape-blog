50076571be9476ea4a065275dd78c061f9fa2f8f
Natalie Smith <thatgalnatalie@gmail.com>
2020-08-21 10:48:45 -0700

    inital commit

---
+++ components/PostCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="post in data" :key="post._id">
+      {{ post.title }}
+      <p>By: {{ post.author.name }}</p>
+
+      <NuxtLink :to="`post/${post._id}`">
+        <button>read</button>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['data']
+}
+</script>
+
+<style></style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-if="getPost">
+      <h1>{{ getPost.title }}</h1>
+      <p>{{ getPost.deck }}</p>
+
+      <NuxtLink :to="`/`">
+        <button>Back Home</button>
+      </NuxtLink>
+    </div>
+
+    <div v-else>
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+import post from '../../apollo/queries/post.gql'
+export default {
+  apollo: {
+    getPost: {
+      query: post,
+      prefetch: ({ route }) => ({ id: route.params.id }),
+      variables() {
+        return { id: this.$route.params.id }
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style></style>
