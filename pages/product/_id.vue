fd7737ec2e22730728665d53b6b1c67048acd8a5
daletom <tom@imgix.com>
2021-09-08 08:45:56 -0700

    adjust styling of product page

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="flex justify-center m-6">
     <div v-if="getProduct">
-      <div class="flex flex-col items-center border rounded-lg bg-blue-100">
+      <div class="items-center bg-white">
+        <SocialHead
+      :title="getProduct.name"
+      :description="getProduct.description"
+      :image="getProduct.image.sourceUrl"
+    />
         <ix-img
         :src="getProduct.image.sourceUrl"
-        width="580"
-        height="580"
+        sizes="(min-width: 799px) 799px, 100vw"
         :imgixParams="{fit:'fill', fill:'solid', fillcolor:'fff', auto:'format', ch:'width,dpr'}"
         :alt="getProduct.name"
         loading="lazy"
-        fixed
-        class="mx-auto crop"
+        class="mx-auto crop border rounded-lg"
         />
       </div>
     <div class="w-full p-5 flex flex-col justify-between">
@@ -54,11 +57,6 @@
     </div>
     </div>
     <div v-else>Loading...</div>
-    <SocialHead
-      :title="getProduct.name"
-      :description="getProduct.description"
-      :image="getProduct.image.sourceUrl"
-    />
   </div>
 </template>
 
@@ -89,59 +87,4 @@ export default {
   display: block;
 }
 
-.card {
-  margin: 20px;
-}
-.card > h1 {
-  font-size: 2rem;
-  color: #5539d2;
-}
-
-#tom {
-  width: 500;
-  height: 300;
-}
-
-.card > p {
-  font-size: 1.6rem;
-}
-
-.card-btn {
-  background-color: #442ab9;
-  border: none;
-  color: #ffffff;
-  outline: none;
-  padding: 12px 40px 10px;
-  position: relative;
-  font-size: 1rem;
-  cursor: pointer;
-  border-radius: 3px;
-}
-
-.card-btn:before,
-.card-btn:after {
-  border: 0 solid transparent;
-  transition: all 0.3s;
-  content: '';
-  height: 0;
-  position: absolute;
-  width: 14px;
-}
-
-.card-btn:before {
-  border-top: 2px solid #ffc107;
-  right: 0;
-  top: -4px;
-}
-
-.card-btn:after {
-  border-bottom: 2px solid #ffc107;
-  bottom: -4px;
-  left: 0;
-}
-
-.card-btn:hover:before,
-.card-btn:hover:after {
-  width: 100%;
-}
 </style>
